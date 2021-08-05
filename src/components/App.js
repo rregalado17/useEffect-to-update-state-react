1733f39beb8f460609302e3ff4f6f71888979577
rregalado17 <rregalado17@gmail.com>
2021-08-04 21:44:15 -0400

    useEffect to update state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from 'react' 
+
+function App() {
+
+    const [doggo, setDoggo] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((res) => res.json())
+            .then((data) => {
+                setDoggo(data.message)
+                setIsLoaded(true)
+            });
+    }, []);
+
+    if(!isLoaded) return <h3>Loading...</h3>
+    return <p><img src={doggo} alt="A Random Dog"/></p>
+}
+
+
+export default App
